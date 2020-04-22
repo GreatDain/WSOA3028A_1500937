d5f7fea6dc113be1231f993a2db3a444e0e74c82
Daingab <47778568+Daingab@users.noreply.github.com>
2020-04-23 00:03:55 +0200

    Many changes.
    Character blogs started. Personal blogs added. Recent blogs navigation implemented.  Annotated wireframes. Other minor adjustments to information about character.

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ console.log(window.document);
 
 */
 
-function Button(){
+/*function Button(){
 
     //access button element
     const doc = document.getElementById("btn");
@@ -82,8 +82,96 @@ function Button(){
     // append button to doc
     doc.appendChild(button);
     
-}
+}*/
+
+let i = 0;
 
 function backbutton(){
     window.history.back();
 }
+
+function nextblogdain(){
+
+    // const doc = document.getElementById('blog1');
+
+    document.getElementById('blog1');
+
+    const bloglinks = [
+        '<a href="Blogs/2020/02/18/blog2.html"> 17.02.2020 - Character Decription </a>',
+        '<a href="Blogs/2020/02/18/blog3.html"> 18.02.2020 - Through the ages of the browser wars </a>',
+        '<a href="Blogs/2020/02/25/blog4.html"> 25.02.2020 - Semantic Markup </a>',
+        '<a href="Blogs/2020/03/10/JavascriptBlog.html">10.03.2020 - Javascript, the "Yes Man" of programming languages </a>',
+        '<a href="Blogs/2020/04/7/FictionalCharacterWritingBlog.html">07.04.2020 - My first experiences with fictional blog writing and character development </a>'
+    ];
+
+    if(i < bloglinks.length-1){
+        i++;
+    }else{
+        i = 0;
+    }
+
+    document.getElementById('blog1').innerHTML = bloglinks[i];
+}
+
+function previousblogdain(){
+
+    // const doc = document.getElementById('blog1');
+
+    document.getElementById('blog1');
+
+    const bloglinks = [
+        '<a href="Blogs/2020/02/18/blog2.html"> 17.02.2020 - Character Decription </a>',
+        '<a href="Blogs/2020/02/18/blog3.html"> 18.02.2020 - Through the ages of the browser wars </a>',
+        '<a href="Blogs/2020/02/25/blog4.html"> 25.02.2020 - Semantic Markup </a>',
+        '<a href="Blogs/2020/03/10/JavascriptBlog.html">10.03.2020 - Javascript, the "Yes Man" of programming languages </a>',
+        '<a href="Blogs/2020/04/7/FictionalCharacterWritingBlog.html">07.04.2020 - My first experiences with fictional blog writing and character development </a>'
+    ];
+
+    if(i <= 0){
+        i = bloglinks.length-1;
+    }else{
+        i--;
+    }
+
+    document.getElementById('blog1').innerHTML = bloglinks[i];
+}
+
+function nextblogjozi(){
+    document.getElementById('joziblog');
+
+    const bloglinks = [
+        '<a href="JoziBlogs/2020/03/15/What-a-DISASTER-1.html"> What a DISASTER - Part 1 </a>',
+        '<a href="JoziBlogs/2020/03/22/Fast-food-and-earthquakes-1.html"> Fast food and earthquakes - Part 1</a>'
+    ]
+
+    if(i < bloglinks.length-1){
+        i++;
+    }else{
+        i = 0;
+    }
+
+    document.getElementById('joziblog').innerHTML = bloglinks[i];
+}
+
+function previousblogjozi(){
+    document.getElementById('joziblog');
+
+    const bloglinks = [
+        '<a href="JoziBlogs/2020/03/15/What-a-DISASTER-1.html"> What a DISASTER - Part 1 </a>',
+        '<a href="JoziBlogs/2020/03/22/Fast-food-and-earthquakes-1.html"> Fast food and earthquakes - Part 1</a>'
+    ]
+
+    if(i <= 0){
+        i = bloglinks.length-1;
+    }else{
+        i--;
+    }
+
+    document.getElementById('joziblog').innerHTML = bloglinks[i];
+}
+
+
+
+
+
+
