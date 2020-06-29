3af0e8fb880b8f051ad47747a89f096991a72bb6
Daingab <47778568+Daingab@users.noreply.github.com>
2020-06-29 11:03:35 +0200

    map popups

--- JavaScript/threewords.js
+++ JavaScript/threewords.js
@@ -20,6 +20,8 @@ function defaultmap(){
     // point.id = "marker";
     map.addControl(new mapboxgl.NavigationControl());
 
+    let popup = new mapboxgl.Popup({ offset: 25 });
+
     // let interest = ['28.0473,-26.2041','28.052729,-26.108843'];
 
     // for(let i = 0; i<interest.length; i++){
@@ -31,78 +33,97 @@ function defaultmap(){
 
     let marker = new mapboxgl.Marker()
     .setLngLat([28.0473,-26.2041])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('The City of Johannesburg'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.052729,-26.108843])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Sandton City'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.009876,-26.23678])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Gold Reef City'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.075969,-25.997318])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Kyalami Race Track and Convention Centre'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.058835,-26.204358])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Maboneng Precinct'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([27.969163,-26.145038])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Northcliff Tower'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([27.767371,-25.925385])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Cradle of Humankind'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([27.844846,-26.086554])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Walter Sisulu National Botanical Gardens'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.083799,-26.156142])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Satyagraha House'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.099554,-25.81219])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Constitution Hill'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.241153,-26.136683])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('O.R Tambo International Airport'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.030567,-26.192904])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('University of the Witwatersrand'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.057292,-26.103345])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Johannesburg Stock Exchange (JSE)'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.038527,-26.204708])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Johannesburg City Library'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.008372,-26.237858])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('The Apartheid Museum'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([27.908024,-26.235217])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Hector Pieterson Memorial and Museum'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.042613,-26.163689])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('South African National Museum of Military History'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.033511,-26.201959])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Market Theatre'))
     .addTo(map);
 
     marker = new mapboxgl.Marker()
     .setLngLat([28.008126,-26.185196])
+    .setPopup(popup = new mapboxgl.Popup({ offset: 25 }).setText('Lindfield Victorian House Museum'))
     .addTo(map);
 
 }
