092d0fc039a5d1800a6c23ebd2b2c38b7c38b711
Daingab <47778568+Daingab@users.noreply.github.com>
2020-06-29 10:41:48 +0200

    added map pins

--- JavaScript/threewords.js
+++ JavaScript/threewords.js
@@ -13,14 +13,98 @@ function defaultmap(){
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [28.0473,-26.2041],
-        zoom: 12
+        zoom: 11
     });
+
+    // let point = document.createElement('div');
+    // point.id = "marker";
+    map.addControl(new mapboxgl.NavigationControl());
+
+    // let interest = ['28.0473,-26.2041','28.052729,-26.108843'];
+
+    // for(let i = 0; i<interest.length; i++){
+    //     let marker = new mapboxgl.Marker()
+    //     .setLngLat(interest[i])
+    //     .addTo(map);
+    //     console.log(i);
+    // };
 
     let marker = new mapboxgl.Marker()
     .setLngLat([28.0473,-26.2041])
     .addTo(map);
 
-    map.addControl(new mapboxgl.NavigationControl());
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.052729,-26.108843])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.009876,-26.23678])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.075969,-25.997318])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.058835,-26.204358])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([27.969163,-26.145038])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([27.767371,-25.925385])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([27.844846,-26.086554])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.083799,-26.156142])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.099554,-25.81219])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.241153,-26.136683])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.030567,-26.192904])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.057292,-26.103345])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.038527,-26.204708])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.008372,-26.237858])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([27.908024,-26.235217])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.042613,-26.163689])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.033511,-26.201959])
+    .addTo(map);
+
+    marker = new mapboxgl.Marker()
+    .setLngLat([28.008126,-26.185196])
+    .addTo(map);
+
 }
 
 function combine3words(){
@@ -53,6 +137,7 @@ function combine3words(){
         nearestPlace = document.getElementById("nearest");
 
         coords.innerHTML+= response.coordinates.lat + ',' + response.coordinates.lng;
+        // coords.innerHTML+= response.coordinates.lng + ',' + response.coordinates.lat;
         nearestPlace.innerHTML+= response.nearestPlace;
 
         mapboxgl.accessToken = 'pk.eyJ1IjoiMTUwMDkzNyIsImEiOiJja2J4cWI0ajEwcTE3MnFtc2Q5ZGZzMDA5In0.bz8yNuJ4cfmz0PqtUftnqA';
